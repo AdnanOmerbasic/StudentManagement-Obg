99e4cb8326053af4d7a90477876a5f9e2d8ac9e4
Adnan Omerbasic <119440057+AdnanOmerbasic@users.noreply.github.com>
2025-10-03 17:50:55 +0200

    ef add instructor + script (#4)

---
+++ Migrations/20251003154705_AddInstructor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentManagement_Obg.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInstructor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "InstructorId",
+                table: "Course",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Instructor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    HireDate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Instructor", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Course_InstructorId",
+                table: "Course",
+                column: "InstructorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Instructor_Email",
+                table: "Instructor",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Course_Instructor_InstructorId",
+                table: "Course",
+                column: "InstructorId",
+                principalTable: "Instructor",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Course_Instructor_InstructorId",
+                table: "Course");
+
+            migrationBuilder.DropTable(
+                name: "Instructor");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Course_InstructorId",
+                table: "Course");
+
+            migrationBuilder.DropColumn(
+                name: "InstructorId",
+                table: "Course");
+        }
+    }
+}
---
+++ artifacts-db/ef/v4_AddInstructor.sql
@@ -0,0 +1,24 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Course] ADD [InstructorId] int NULL;
+
+CREATE TABLE [Instructor] (
+    [Id] int NOT NULL IDENTITY,
+    [FirstName] nvarchar(50) NOT NULL,
+    [LastName] nvarchar(50) NOT NULL,
+    [Email] nvarchar(100) NOT NULL,
+    [HireDate] date NOT NULL,
+    CONSTRAINT [PK_Instructor] PRIMARY KEY ([Id])
+);
+
+CREATE INDEX [IX_Course_InstructorId] ON [Course] ([InstructorId]);
+
+CREATE UNIQUE INDEX [IX_Instructor_Email] ON [Instructor] ([Email]);
+
+ALTER TABLE [Course] ADD CONSTRAINT [FK_Course_Instructor_InstructorId] FOREIGN KEY ([InstructorId]) REFERENCES [Instructor] ([Id]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20251003154705_AddInstructor', N'9.0.9');
+
+COMMIT;
+GO
+
