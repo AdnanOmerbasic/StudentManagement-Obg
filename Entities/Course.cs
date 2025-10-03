ff9320fdf784e132413808491db29cb33f6ee38c
Adnan Omerbasic <119440057+AdnanOmerbasic@users.noreply.github.com>
2025-10-03 16:39:20 +0200

    ef initial schema + script

---
+++ Entities/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement_Obg.Entities
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public Course()
+        {
+
+        }
+    }
+}
---
+++ Entities/Enrollment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement_Obg.Entities
+{
+    public class Enrollment
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+        public int? Grade { get; set; }
+        public Enrollment()
+        {
+
+        }
+    }
+}
