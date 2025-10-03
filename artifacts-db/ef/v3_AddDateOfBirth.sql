21599704f9c414c6363e0c67f81a11b42d6f17fc
Adnan Macanovic Omerbasic <AdnanOmerbasic99@gmail.com>
2025-10-03 17:31:12 +0200

    ef add date of birth + script

---
+++ artifacts-db/ef/v3_AddDateOfBirth.sql
@@ -0,0 +1,9 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Student] ADD [DateOfBirth] date NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20251003152204_AddDateOfBirth', N'9.0.9');
+
+COMMIT;
+GO
+
