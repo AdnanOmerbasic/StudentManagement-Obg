f911e6bd125e6943c3c0046d1a64dc2e4be8db6a
Adnan Omerbasic <119440057+AdnanOmerbasic@users.noreply.github.com>
2025-10-03 17:31:48 +0200

    ef add date of birth + script (#3)

--- Entities/Student.cs
+++ Entities/Student.cs
@@ -12,6 +12,7 @@ namespace StudentManagement_Obg.Entities
         public string FirstName { get; set; }
         public string? MiddleName { get; set; }
         public string LastName { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
         public string Email { get; set; }
         public DateOnly EnrollmentDate { get; set; }
 
