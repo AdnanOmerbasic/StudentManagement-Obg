58f8caf4dbc7cf4fa288f62e62e6b96fced139f5
Adnan Omerbasic <119440057+AdnanOmerbasic@users.noreply.github.com>
2025-10-03 17:04:18 +0200

    ef add middlename + script (#2)

---
+++ artifacts-db/ef/v2_AddMiddleName.sql
@@ -0,0 +1,9 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Student] ADD [MiddleName] nvarchar(50) NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20251003145953_AddMiddleName', N'9.0.9');
+
+COMMIT;
+GO
+
